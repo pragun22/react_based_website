e2b65214d6e53bf197c1521f94516a6441704325
pragun22 <pragun.saxena@students.iiit.ac.in>
2019-07-04 14:55:39 +0530

    added work experiences

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 import Intro from './components/Intro/intro.js'
 import Timejour from './components/timejour/Timejour.js'
-// import Timeline from './components/Timeline/timeline';
+import Works from './components/Works/works.js'
 function App() {
   return (
     <div>
     <Intro/>
+    <Works/>
     <Timejour/>
     </div>
   );
--- src/components/timejour/Timejour.js
+++ src/components/timejour/Timejour.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './timejour.css';
 import Timeline from '../Timeline/timeline'
 import {StyleRoot} from "radium";
+import CssBaseline from '@material-ui/core/CssBaseline';
 class Timejour extends React.Component {
     render(){
         return(
             <div>
+                <CssBaseline/>
             <StyleRoot className="time">
                     <Timeline >
                         <div icon='1992'>started school - 19XX</div>
